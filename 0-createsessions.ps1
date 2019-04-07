22a54132bf5532230d487d45a5f873b7c97b3702
Roman Stadlmair <roman.stadmair@live.com>
2019-04-07 12:37:56 +0200

    remoting - 1

--- 0-createsessions.ps1
+++ 0-createsessions.ps1
@@ -1,13 +1,13 @@
 # Windows Box "Striterax"
 $acred = Get-Credential -UserName 'Thegalaxy\a-roman'
 $acred.Password|Set-Clipboard
-$win = New-PSSession -HostName 'striterax.thegalaxy.local' -UserName $acred.Username
+$win = New-PSSession -HostName 'striterax.thegalaxy.local' -Name 'win' -UserName 'a-roman'
+#>
 
 # Ubuntu Box 'Jumpbox'
-$lin = New-PSSession -HostName '172.16.110.20' -UserName 'marvin'
-
+$lin = New-PSSession -HostName '172.16.110.20' -Name 'lin' -UserName marvin
 
 # Local mac session
-$mac = New-PSSession -HostName localhost
+$mac = New-PSSession -HostName '172.16.10.27' -Name 'mac' -username psuser
 # Achtung - Bug mit CLS auf Linux dort:
 $env:TERM = 'vt100'
--- 1-environment.ps1
+++ 1-environment.ps1
@@ -1,22 +1,45 @@
 # Systemumgebung ist in Betriebssystemen unterschiedlich
 
 ## Wo bin ich ? - nicht ganz eindeutig .?
-[Environment]::OSVersion
-[Environment]::OSVersion.Platform
-[Environment]::Is64BitOperatingSystem
+$script = {
+    " "
+    $OsVersion = [Environment]::OSVersion
+    "OS Version: $OsVersion"
+    $OsPlatform = [Environment]::OSVersion.Platform
+    "OS Platform: $OsPlatform"
+    $is64BitOS = [Environment]::Is64BitOperatingSystem
+    "Is 64 BitOS: $is64BitOS"
+}
+invoke-command -Session $win,$lin,$mac -ScriptBlock $script
 
 ## Windows => sogar Autocomplete von $env
-invoke-command -Session $win,$lin -ScriptBlock {
+$Env:COMPUTERNAME
+$env:USER
+
+$script = {
+    " "
     "Computername is: $env:COMPUTERNAME"
+    "User is: $env:USER"
     "Text mit neuer `n Zeile dazwischen"
     "Tempdrive is: $env:TEMP"
 }
+invoke-command -Session $win,$lin,$mac -ScriptBlock $script
 
 # Stattdessen: [Environment]
-[Environment]::UserName
-[Environment]::MachineName
-[Environment]::NewLine
-[IO.Path]::GetTempPath()
+$script = {
+    $DotNetMachineName = [Environment]::MachineName
+    "Computername is: $DotNetmachinename"
+    
+    $DotNetusername = [Environment]::UserName
+    "User is: $Dotnetusername"
+    
+    $DotNetNewLine = [Environment]::NewLine
+    "Text mit neuer $DotNetNewLine Zeile dazwischen"
+    
+    $EnvTempPath = [IO.Path]::GetTempPath()
+    "Tempdrive is: $envTempPath"
+}
+invoke-command -Session $win,$lin,$mac -ScriptBlock $script
 
 # Pfad Variable
 $env:Path -split ';'
